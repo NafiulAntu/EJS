36c1ec832af7ebf40e5e2753a3296a1a794cdabf
nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-02-27 00:55:02 +0600

    Add files via upload

---
+++ prac2.js
@@ -0,0 +1,26 @@
+// var tweet = prompt("Compose your tweet: ");
+// var tweetCount = tweet.lenght;
+// alert("you have written " + tweetCount + "characters, you have "+ (140 - tweetCount) + " characters remaining. " );
+
+//var name = "Nafiul";
+//name.slice(,6);
+
+// var tweet = prompt("Compose your tweet: ");
+// var tweetCount = tweet.slice(0,140);
+// alert(tweetCount);
+
+//alert(prompt("Compose your tweet : ").slice(0,140));
+
+// var name = prompt("whats is your name ?: ")
+// var firstChar = name.slice(0,1);
+
+// var upperCaseFirstChar = firstChar.toUpperCase();
+
+// var restOfName = name.slice(1,name.length);
+
+// var capitilisedName = upperCaseFirstChar + restOfName;
+
+// alert("Hello, " + capitilisedName);
+
+//name = name.toUpperCase();
+//name =name.toLowerCase();
