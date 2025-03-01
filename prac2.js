f2052b0f7818958ed18f2ed3a3897efae8ab3695
nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-03-02 01:57:50 +0600

    Update prac2.js

--- prac2.js
+++ prac2.js
@@ -1,26 +1,26 @@
-// var tweet = prompt("Compose your tweet: ");
-// var tweetCount = tweet.lenght;
-// alert("you have written " + tweetCount + "characters, you have "+ (140 - tweetCount) + " characters remaining. " );
+var tweet = prompt("Compose your tweet: ");
+var tweetCount = tweet.lenght;
+alert("you have written " + tweetCount + "characters, you have "+ (140 - tweetCount) + " characters remaining. " );
 
-//var name = "Nafiul";
-//name.slice(,6);
+var name = "Nafiul";
+name.slice(,6);
 
-// var tweet = prompt("Compose your tweet: ");
-// var tweetCount = tweet.slice(0,140);
-// alert(tweetCount);
+var tweet = prompt("Compose your tweet: ");
+var tweetCount = tweet.slice(0,140);
+alert(tweetCount);
 
-//alert(prompt("Compose your tweet : ").slice(0,140));
+alert(prompt("Compose your tweet : ").slice(0,140));
 
-// var name = prompt("whats is your name ?: ")
-// var firstChar = name.slice(0,1);
+var name = prompt("whats is your name ?: ")
+var firstChar = name.slice(0,1);
 
-// var upperCaseFirstChar = firstChar.toUpperCase();
+var upperCaseFirstChar = firstChar.toUpperCase();
 
-// var restOfName = name.slice(1,name.length);
+var restOfName = name.slice(1,name.length);
 
-// var capitilisedName = upperCaseFirstChar + restOfName;
+var capitilisedName = upperCaseFirstChar + restOfName;
 
-// alert("Hello, " + capitilisedName);
+alert("Hello, " + capitilisedName);
 
-//name = name.toUpperCase();
-//name =name.toLowerCase();
+name = name.toUpperCase();
+name =name.toLowerCase();
