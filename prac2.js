74774ce916e9c8c90a427d91933bb5cdb62ec19b
nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-03-02 02:01:29 +0600

    Update prac2.js

--- prac2.js
+++ prac2.js
@@ -11,18 +11,5 @@ alert(tweetCount);
 
 alert(prompt("Compose your tweet : ").slice(0,140));
 
-var name = prompt("whats is your name ?: ")
-var firstChar = name.slice(0,1);
-
-var upperCaseFirstChar = firstChar.toUpperCase();
-
-var restOfName = name.slice(1,name.length);
-
-var capitilisedName = upperCaseFirstChar + restOfName;
-
-alert("Hello, " + capitilisedName);
-
-name = name.toUpperCase();
-name =name.toLowerCase();
 
 
