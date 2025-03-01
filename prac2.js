a3361ccd970fd3912ed888d346a2b21cbc20bbe1
nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-03-02 01:59:10 +0600

    Update prac2.js

--- prac2.js
+++ prac2.js
@@ -24,3 +24,23 @@ alert("Hello, " + capitilisedName);
 
 name = name.toUpperCase();
 name =name.toLowerCase();
+
+
+function isLeap(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return "leap Year.";
+      } else {
+        return "Not leap year.";
+      }
+    }
+    else {
+      return "leap year.";
+    }
+  }
+  else {
+    return "Not leap year.";
+  }
+}
+
