9c3114ceca2017abf5efd3a1b0f9f1162f763f84
Nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-04-23 18:41:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.set("/", (req, res) => {
+  const today = new Date("July 21, 1983 01:15:00");
+  const day = today.getDay();
+  //console.log(day);
+
+  let type = "a weekday";
+  let adv = "Its time to work hard";
+
+  if (day === 0 || day === 6) {
+    type = "a weekend";
+    adv = "Its time to relax";
+  }
+
+  res.render("index.js", {
+    dayType: "a weekday",
+    advice: "Its time to work hard",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
